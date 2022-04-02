c09cc461469b210eec627c3afbe1eb5efbdf96b6
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2022-04-02 17:56:01 +0200

    refactor

--- edc_sync_data_report/classes/client_collect_summary_data.py
+++ edc_sync_data_report/classes/client_collect_summary_data.py
@@ -1,22 +1,8 @@
-from edc_sync_data_report.models import SyncModels, ClientSyncSummary
-from django.apps import apps
+from edc_sync_data_report.classes.summary_data import SummaryData
+from edc_sync_data_report.models import ClientSyncSummary
 
 
-class ClientCollectSummaryData(object):
-
-    def count_by_app_label_and_model_name(self, app_label, model_name):
-        model_cls = apps.get_model(app_label, model_name)
-        return model_cls.objects.count()
-
-    def build_summary(self):
-        data = {}
-        sync_models = SyncModels.objects.all()
-
-        for sync_model in sync_models:
-            key = f'{sync_model.app_label}__{sync_model.model_name}'
-            model_data = {f'{key}': self.count_by_app_label_and_model_name(sync_model.app_label, sync_model.model_name)}
-            data.update(model_data)
-        return data
+class ClientCollectSummaryData(SummaryData):
 
     def create_summary_data(self):
         if len(self.build_summary()) > 0:
