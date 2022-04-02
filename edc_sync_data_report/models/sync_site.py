649e56029addbcbb00de1ddf8291cde42616ffd8
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2022-04-02 10:43:57 +0200

    site model

---
+++ edc_sync_data_report/models/sync_site.py
@@ -0,0 +1,60 @@
+from django.db import models
+from edc_base.model_mixins import BaseUuidModel
+
+from edc_base.sites import SiteModelMixin
+from edc_search.model_mixins import SearchSlugManager
+from edc_search.model_mixins import SearchSlugModelMixin as Base
+
+
+class SyncSiteManager(SearchSlugManager, models.Manager):
+
+    def get_by_natural_key(self, identifier):
+        return self.get(identifier=identifier)
+
+
+class SearchSlugModelMixin(Base):
+
+    def get_search_slug_fields(self):
+        fields = super().get_search_slug_fields()
+        return fields
+
+    class Meta:
+        abstract = True
+
+
+class SyncSite(SiteModelMixin, SearchSlugModelMixin, BaseUuidModel):
+
+    # identifier_cls = ExportIdentifier
+
+    identifier = models.CharField(
+        verbose_name="Site Identifier",
+        max_length=36,
+        unique=True,
+        editable=False)
+
+    study = models.CharField(
+        max_length=100, blank=True,
+        default='flourish')
+
+    description = models.CharField(max_length=255, blank=True)
+
+    valid_from = models.DateField(
+        verbose_name="Report start date",
+        null=True,
+        blank=True)
+
+    valid_to = models.DateField(
+        verbose_name="Report start date",
+        null=True,
+        blank=True)
+
+    def __str__(self):
+        return f'{self.identifier}'
+
+    def natural_key(self):
+        return self.identifier
+
+    def get_search_slug_fields(self):
+        fields = super().get_search_slug_fields()
+        fields.append('identifier')
+        return fields
--- edc_sync_data_report/settings.py
+++ edc_sync_data_report/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'edc_sync_data_report.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
