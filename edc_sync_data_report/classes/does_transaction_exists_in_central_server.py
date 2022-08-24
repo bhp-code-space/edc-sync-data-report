63dde343d15b1e7589ccf1edb06294c1716adc27
Itumeleng Mosweu <imosweuk@gmail.com>
2022-08-24 18:12:44 +0200

    fix: change history record query to primary key

--- edc_sync_data_report/classes/does_transaction_exists_in_central_server.py
+++ edc_sync_data_report/classes/does_transaction_exists_in_central_server.py
@@ -8,10 +8,11 @@ class DoesTransactionExistsInCentralServer:
     """
         Checks if the record exists in central server given primary key.
     """
+
     def sync_data_check(self, data):
         missing_records = []
         for record in data:
-            model_cls = django_apps.get_model(model_name=record["model_name"], app_label=record["app_label"]) #
+            model_cls = django_apps.get_model(model_name=record["model_name"], app_label=record["app_label"])  #
             # Fixme handle a case where model does exits
             is_historical_model = False
             try:
@@ -21,7 +22,7 @@ class DoesTransactionExistsInCentralServer:
                 pass
             try:
                 if is_historical_model:
-                    model_cls.objects.get(history_id=record["history_id"])
+                    model_cls.objects.get(history_id=record["primary_key"])
                 else:
                     model_cls.objects.get(id=record["primary_key"])
             except ObjectDoesNotExist:
